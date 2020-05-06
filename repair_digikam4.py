e39a0afcfb744bc118b020d682f240879d34555e
Peter Teuben <teuben@gmail.com>
2020-05-05 23:56:55 -0400

    python3, but not re-tested after the albumRoot change

--- repair_digikam4.py
+++ repair_digikam4.py
@@ -18,23 +18,23 @@ db2 = sys.argv[3]
 d2  = int(sys.argv[4])
 
 
-print "# ",db1,d1
+print("# ",db1,d1)
 con1 = lite.connect(db1)
 cur1 = con1.cursor()
 cur1.execute('SELECT * FROM AlbumRoots')
 ar1 = cur1.fetchall()
 for ar in ar1:
     if ar[0] == d1:
-        print "# From: ",ar[0], ar[1], ar[2], ar[3], ar[5]
+        print("# From: ",ar[0], ar[1], ar[2], ar[3], ar[5])
 
-        print "# ",db2,d2
+        print("# ",db2,d2)
 con2 = lite.connect(db2)
 cur2 = con2.cursor()
 cur2.execute('SELECT * FROM AlbumRoots')
 ar2 = cur2.fetchall()
 for ar in ar2:
     if ar[0] == d2:
-        print "# To:   ",ar[0], ar[1], ar[2], ar[3], ar[5]
+        print("# To:   ",ar[0], ar[1], ar[2], ar[3], ar[5])
         path_to = ar[5]
 
 # sys.exit(0)        
@@ -53,12 +53,12 @@ for row in rows:
     r2 = cur2.fetchall()
     if len(r2) == 1:
         #print 'old',r1[0][1],r1[0][2],r1[0][3],r1[0][4]
-        print 'fix',row[1],row[2],row[3],line1(row[4]),'<-',r2[0][4]
+        print('fix',row[1],row[2],row[3],line1(row[4]),'<-',r2[0][4])
         cmd = 'UPDATE Albums set caption = "%s" where relativePath = "%s"' % (line1(row[4]),row[2])
         #print "CMD:",cmd
         con2.execute(cmd)
     else:
-        print "BAD len"
+        print("BAD len")
 
 
 # when all is well, we commit (during development of the script we comment this out)
@@ -66,7 +66,7 @@ for row in rows:
 con2.commit()
 n = con2.total_changes
 # and report
-print "Total number of rows updated in %s : %d" % ( db2, n)
+print("Total number of rows updated in %s : %d" % ( db2, n))
 
 
 
